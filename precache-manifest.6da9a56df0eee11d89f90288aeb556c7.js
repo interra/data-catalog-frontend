64b1b5e84c0f244912c698938a79c4f435ce61f5
acouch <acinternets@gmail.com>
2019-02-11 12:18:12 -0500

    Updates

--- precache-manifest.6da9a56df0eee11d89f90288aeb556c7.js
+++ precache-manifest.6da9a56df0eee11d89f90288aeb556c7.js
@@ -8,8 +8,8 @@ self.__precacheManifest = [
     "url": "/data-catalog-frontend/static/js/runtime~main.51127487.js"
   },
   {
-    "revision": "500a4a1049b9faf5c115",
-    "url": "/data-catalog-frontend/static/js/main.500a4a10.chunk.js"
+    "revision": "62dea95887f432fe52d1",
+    "url": "/data-catalog-frontend/static/js/main.62dea958.chunk.js"
   },
   {
     "revision": "a5790c218675ccc9b761",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/data-catalog-frontend/static/css/1.ad5791d0.chunk.css"
   },
   {
-    "revision": "a8dafb374ca6172c81222827c67b24cf",
+    "revision": "a072cbba8c98e5d5f0e7b0b1cf6de027",
     "url": "/data-catalog-frontend/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/data-catalog-frontend/precache-manifest.0d3da4e989ff9a8a57cad4c2d25835be.js"
+  "/data-catalog-frontend/precache-manifest.6da9a56df0eee11d89f90288aeb556c7.js"
 );
 
 workbox.clientsClaim();
